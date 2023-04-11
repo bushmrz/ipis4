bd997463b51ba43a385136d5800349a7c50a6fc1
bushmrzzz <buuushmrz@mail.ru>
2023-04-11 22:22:07 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
-void main(){
-name = "";
-std::cout<<"Enter ur name: ";
-std::cin>>name;
-std::cout<<"Hello world from " + name;
+void main()
+{
+    string name = "";
+    std::cout<<"Enter ur name: ";
+    std::cin>>name;
+    std::cout<<"Hello world from " + name;
 }
